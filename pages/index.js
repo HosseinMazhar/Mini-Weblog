58cd861236d2c9f61156ae4c47772321d24d9a23
Alireza.N <Alireza.N@behinrahkar.com>
2023-06-21 16:54:08 +0330

    cdx-3-adding-global-state-with-zustand

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,16 @@ import SearchInput from "@/components/searchInput";
 import Sorter from "@/components/sorter";
 import PostsContainer from "@/components/postsContainer";
 import getPosts from "./api/posts";
+import { usePostsStore } from "@/store/postsStore";
 
 export default function Home() {
-  const {data, isLoading, isFetching} = useQuery("posts", getPosts);
+  debugger;
+  const {data} = useQuery("posts", getPosts);
+  const { setPosts } = usePostsStore();
+  useQuery('posts', getPosts, {
+    onSuccess: (data) => setPosts(data),
+    initialData: data,
+  })
   return (
     <div id="app-container" className="w-full min-h-screen box-border overflow-hidden bg-slate-800 flex flex-col items-center p-10 gap-6">
       <SearchInput/>
