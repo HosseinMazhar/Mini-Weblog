fc64c01aacc9b65fb61f1afdcf298b0f56586600
unknown <hosseinmazhar1053@gmail.com>
2023-06-26 02:58:53 -0700

    cdx-all

--- components/sorter.jsx
+++ components/sorter.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { usePostsStore } from '@/store/postsStore'
 
 export default function Sorter() {
+  const sortHandler = (e) => {
+    switch (e.target.value) {
+      case 'asc':
+        usePostsStore.getState().sortPosts((a,b) => a.id - b.id)
+        break;
+      case 'dsc':
+        usePostsStore.getState().sortPosts((a,b) => b.id - a.id)
+      default:
+        break;
+    }
+  }
   return (
     <div id="header" className="w-2/3 box-border h-10 border-b-2 border-slate-300 flex flex-row gap-2">
-        <select id="salect-sorting-type" className="mb-2 rounded">
-            <option selected>sort</option>
-            <option>up to down</option>
-            <option>down to up</option>
+        <select id="salect-sorting-type" className="mb-2 rounded" onChange={sortHandler}>
+          <option value={""}>sort</option>
+          <option value={"asc"}>up to down</option>
+          <option value={"dsc"}>down to up</option>
         </select>
     </div>
   )
